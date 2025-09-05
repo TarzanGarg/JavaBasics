e859e27b30ce5b6728239bdfa280fb0229911069
Tarzan Garg <tarzan6239@gmail.com>
2025-09-05 17:43:45 -0400

    Question12 resolved

--- src/main/java/practicequestions/Question12.java
+++ src/main/java/practicequestions/Question12.java
@@ -2,17 +2,30 @@ package practicequestions;
 //Custom Exception
 //Create a custom exception InvalidAgeException. In a Voting class, throw this exception if the age is less than 18.
 class Voting{
-    Voting(int a) {
-        assert a >=18 : "InvalidAgeException";
+    Voting(int a) throws InvalidAgeException{
+        if (a<18){
+            throw new InvalidAgeException("You are not ready for vote");
+        }
         System.out.println("You are ready to vote");
     }
 }
+
+class InvalidAgeException extends Exception{
+    public InvalidAgeException(String message){
+        super(message);
+    }
+}
+
 public class Question12 {
     public static void main(String args[]){
-        Voting v1 = new Voting(23);
-        Voting v2 = new Voting(32);
-        Voting v3 = new Voting(7);
-        Voting v4 = new Voting(12);
-        Voting v5 = new Voting(24);
+        try {
+            Voting v1 = new Voting(23);
+            Voting v2 = new Voting(32);
+            Voting v3 = new Voting(71);
+            Voting v4 = new Voting(12);
+            Voting v5 = new Voting(24);
+        } catch (InvalidAgeException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
